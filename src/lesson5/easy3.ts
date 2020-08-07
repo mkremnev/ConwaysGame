9f24574931fd944143c5ef7160c1aca2836442ca
Maxim Kremnev <maxim.kremnev@itqc.ru>
2020-08-07 11:02:19 +0300

    Lesson 5: formation type for lesson

---
+++ src/lesson5/easy3.ts
@@ -0,0 +1,15 @@
+// Задание первого уровня 3
+// Есть общая функция omit которая удаляет поле из объекта и возвращает его без этого поля
+// Нужно заменить FIXME на соответствующий тип
+
+type FIXME = {
+	[key in any]: any;
+};
+
+export const omit = <T extends Record<any, any>, K extends keyof T>(
+	obj: T,
+	keyToOmit: K,
+): FIXME => {
+	const { [keyToOmit]: _, ...withoutKey } = obj;
+	return withoutKey;
+};
