3a719653c848a57be6f58c4588586115de431de4
Maxim Kremnev <maxim.kremnev@itqc.ru>
2020-08-28 20:15:14 +0300

    Edit component InputName

--- src/components/Users/InputName.tsx
+++ src/components/Users/InputName.tsx
@@ -1,17 +1,41 @@
-import React, { FC } from 'react';
+import React, { FC, useState, FormEvent } from 'react';
+import { Input } from '../Interfaces/Input/Input';
+import { Button } from '../Interfaces/Button/Button';
+import styled from '@emotion/styled';
 
-export type formType = {
-	onSubmit: (ev: React.FormEvent) => void;
-};
+const InputNameWrapper = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	margin-bottom: 10px;
+`;
 
-export const InputName: FC<formType> = ({ onSubmit, children }) => {
+export const InputName: FC = () => {
+	const [name, setName] = useState('Jon Doe');
+
+	const onSubmit = (ev: FormEvent) => {
+		ev.preventDefault();
+	};
+
 	return (
-		<form onSubmit={onSubmit}>
-			<fieldset>
-				<legend>Введите свое имя</legend>
-				{children}
-				<button>Start</button>
-			</fieldset>
-		</form>
+		<InputNameWrapper>
+			<form onSubmit={onSubmit}>
+				<fieldset>
+					<legend>Введите свое имя</legend>
+					<Input
+						type={'text'}
+						onChange={(e) => setName(e.target.value)}
+						name={'UserName'}
+						value={name}
+					/>
+					<Button
+						text={'Отправить'}
+						onClick={onSubmit as () => void}
+					/>
+				</fieldset>
+			</form>
+			<h3>{name}</h3>
+		</InputNameWrapper>
 	);
 };
