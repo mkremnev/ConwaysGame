009683497c7051d97c29bd984ef396aa089bf6c2
Maxim Kremnev <m.kremnev@netlinux.ru>
2020-09-04 00:31:18 +0300

    Settings for testing

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
 module.exports = {
+	clearMocks: true,
 	coverageDirectory: 'coverage',
 	transform: {
 		'^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
 	},
 	moduleNameMapper: {
 		'\\.(css|less)$': '<rootDir>/settings/__mocks__/styleMock.js',
+		'^@/(.*)$': '<rootDir>/src/$1',
 	},
 	cacheDirectory: 'tmp/test',
 	moduleDirectories: ['node_modules', 'src'],
