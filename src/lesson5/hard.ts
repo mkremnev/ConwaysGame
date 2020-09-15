37713f03e14200ce9e9c458e4cf618220b896093
Maxim Kremnev <m.kremnev@netlinux.ru>
2020-09-15 14:03:14 +0300

    Added latest files

---
+++ src/lesson5/hard.ts
@@ -0,0 +1,14 @@
+type FIXME = any;
+
+// Это сложное (если не супер сложное) задание
+// Задача состоит в том что написать калькулято для натуральных чисел, но только на типах!
+// Ниже приведена заготовка
+// Хочется поддержки сложения и вычитания, если хочется еще челленджа, то деление и умножение
+// Из-за ограничений глубины вычислений поддержать все натуральные числа не получится, это нормально
+type Equals<A, B> = A extends B ? (B extends A ? 'success' : never) : never;
+
+type Add<A, B> = FIXME;
+type Subtract<A, B> = FIXME;
+
+export type OnePlusOneTest = Equals<Add<1, 1>, 2>;
+export type TwoMinusOneTest = Equals<Subtract<2, 1>, 1>;
---
+++ src/lesson5/medium3.ts
@@ -0,0 +1,17 @@
+type FIXME = any;
+
+const orderStates = [
+	'initial',
+	'inWork',
+	'buyingSupplies',
+	'producing',
+	'fullfilled',
+] as const;
+
+type OrderState = typeof orderStates[number];
+
+// Hint: type guards
+export const getUserOrderStates = (orderStates: OrderState[]): FIXME =>
+	orderStates.filter(
+		(state) => state !== 'buyingSupplies' && state !== 'producing',
+	);
