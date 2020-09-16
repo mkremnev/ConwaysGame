2ae001b15e5b84c56a58f81abe32ddb5c0dc604a
Maxim Kremnev <m.kremnev@netlinux.ru>
2020-09-16 22:57:40 +0300

    Creating action with toolkit

--- src/rdx/actions.ts
+++ src/rdx/actions.ts
@@ -1,47 +1,11 @@
-export const SET_CELL = 'SET_CELL';
-export const CLEAR_BOARD = 'CLEAR_BOARD';
-export const UPDATE_BOARD = 'UPDATE_BOARD';
-export const CHANGE_SPEED = 'CHANGE_SPEED';
-export const GAME_RUN = 'GAME_RUN';
-export const IS_GAME = 'IS_GAME';
+import { createAction } from '@reduxjs/toolkit';
 
 export type Coordinates = { x: number; y: number };
 export type SpeedState = number | string;
 
-export function setFill(payload: Coordinates) {
-	return {
-		type: SET_CELL,
-		payload,
-	};
-}
-
-export function clearBoard() {
-	return {
-		type: CLEAR_BOARD,
-	};
-}
-
-export function updateBoard() {
-	return {
-		type: UPDATE_BOARD,
-	};
-}
-
-export function changeSpeed(payload: SpeedState) {
-	return {
-		type: CHANGE_SPEED,
-		payload,
-	};
-}
-
-export function gameRun() {
-	return {
-		type: GAME_RUN,
-	};
-}
-
-export function isGame() {
-	return {
-		type: IS_GAME,
-	};
-}
+export const setCell = createAction<Coordinates>('setCell');
+export const clearBoard = createAction<void>('clearBoard');
+export const updateBoard = createAction<void>('updateBoard');
+export const changeSpeed = createAction<SpeedState>('updateBoard');
+export const gameRun = createAction<void>('gameRun');
+export const isGame = createAction<void>('isGame');
