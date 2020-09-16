eaac2e4cfd736bd15eebfefa7bf2930fe0291b80
Maxim Kremnev <m.kremnev@netlinux.ru>
2020-09-17 00:55:15 +0300

    Editing action

--- src/rdx/actions.ts
+++ src/rdx/actions.ts
@@ -6,6 +6,6 @@ export type SpeedState = number | string;
 export const setCell = createAction<Coordinates>('setCell');
 export const clearBoard = createAction<void>('clearBoard');
 export const updateBoard = createAction<void>('updateBoard');
-export const changeSpeed = createAction<SpeedState>('updateBoard');
+export const changeSpeed = createAction<SpeedState>('changeSpeed');
 export const gameRun = createAction<void>('gameRun');
 export const isGame = createAction<void>('isGame');
