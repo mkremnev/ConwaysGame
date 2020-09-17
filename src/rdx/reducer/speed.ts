c36cef73271033292c4a82fd6be5c620f59e35c5
Maxim Kremnev <m.kremnev@netlinux.ru>
2020-09-17 11:51:29 +0300

    Edeting type value

--- src/rdx/reducer/speed.ts
+++ src/rdx/reducer/speed.ts
@@ -2,11 +2,11 @@ import { createReducer } from '@reduxjs/toolkit';
 import * as actions from '@/rdx/actions';
 
 export type SpeedState = {
-	value: number;
+	value: string;
 };
 
 const defaultState: SpeedState = {
-	value: 500,
+	value: '500',
 };
 
 export const speed = createReducer<SpeedState>(defaultState, {
