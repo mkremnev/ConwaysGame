810cc07de8dceae90b3c4042b6750100391f9a9f
Maxim Kremnev <m.kremnev@netlinux.ru>
2020-09-22 07:44:28 +0300

    Settings saga middleware

--- src/rdx/store.ts
+++ src/rdx/store.ts
@@ -1,16 +1,25 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { reducer } from './reducer/index';
+import createSagaMiddleware from 'redux-saga';
+import { helloSaga } from '@/sagas/saga';
+
+const SagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
 	reducer,
-	middleware: getDefaultMiddleware({
-		serializableCheck: {
-			ignoredActions: [
-				'field/clearBoard',
-				'field/updateBoard',
-				'field/isGame',
-				'gameRun',
-			],
-		},
-	}),
+	middleware: [
+		...getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [
+					'field/clearBoard',
+					'field/updateBoard',
+					'field/isGame',
+					'gameRun',
+				],
+			},
+		}),
+		SagaMiddleware,
+	],
 });
+
+SagaMiddleware.run(helloSaga);
