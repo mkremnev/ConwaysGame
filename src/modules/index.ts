1272ccfca8ad0e84c262b6f4dfdc0e5bd53657ff
Maxim Kremnev <m.kremnev@netlinux.ru>
2020-09-25 10:36:35 +0300

    Export modules

---
+++ src/modules/index.ts
@@ -0,0 +1,5 @@
+import { InterfaceLayout } from './Interfaces/Interfaces';
+
+export { GameOfLifeWithRedux } from './GameOfLifeWithRedux/GameOfLifeWithRedux';
+export { GameOfLifeProto } from './Main/GameOfLifeProto';
+export { InterfaceLayout } from './Interfaces/Interfaces';
