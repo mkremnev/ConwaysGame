e491f2cfdfe1c90634cac555dfbfb7ac456c5103
Maxim Kremnev <m.kremnev@netlinux.ru>
2020-10-14 21:42:52 +0300

    Editing component Interfaces

---
+++ src/components/Controls/types.ts
@@ -0,0 +1,9 @@
+import { inputAttributes } from '@/components/Interfaces/Input';
+import { ButtonType } from '@/components/Interfaces/Button/Button';
+
+export type ListButton = {
+	button1: ButtonType;
+	button2: ButtonType;
+	button3: ButtonType;
+	input?: inputAttributes;
+};
